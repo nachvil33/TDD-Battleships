595e2652ca051c8cfbcd3c75f937fd5aaf9edf9f
nachvil33 <nachvil33>
2024-01-26 04:14:39 -0600

    Changes for structure and dependencies used, update for archives generated with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './/src/index.ts',
+  entry: './src/index.ts', // Corregido el path de la entrada
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -16,6 +16,14 @@ module.exports = {
         test: /\.ts$/,
         use: 'ts-loader',
         exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
